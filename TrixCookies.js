d37d2c85f54461e3e2f18a689b43770c2b664f48
Alex Trix <trixter9994@gmail.com>
2019-10-17 15:44:02 -0400

    TrixCookies v0.1.1 dev

--- TrixCookies.js
+++ TrixCookies.js
@@ -3,23 +3,29 @@ if (TrixCookies === undefined) var TrixCookies = {};
 if (typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 
 TrixCookies.launch = function(){
-var iconsURL = 'https://klattmose.github.io/CookieClicker/img/customIcons.png';
-TrixCookies.version = "0.1";
-TrixCookies.GameVersion = "2.021";
+	var iconsURL = 'https://cdn.jsdelivr.net/gh/Trixter9994/TrixCookies@dev/images/customIcons.png';
+	TrixCookies.version = "0.1";
+	TrixCookies.GameVersion = "2.021";
+	TrixCookies.name= "2.021";
+	//CCSE.ConfirmGameVersion(TrixCookies.name, TrixCookies.version, TrixCookies.GameVersion);
 	
-//Research needed to use this. 
-/*Game.customOptionsMenu.push(function(){
-	CCSE.AppendCollapsibleOptionsMenu("TrixCookies", "Test");
-});*/
+	//Research needed to use this. 
+	/*Game.customOptionsMenu.push(function(){
+		CCSE.AppendCollapsibleOptionsMenu("TrixCookies", "Test");
+	});*/
 
-Game.customStatsMenu.push(function(){
-	CCSE.AppendStatsVersionNumber("TrixCookies", TrixCookies.version);
-});'z
-    TrixCookies.isLoaded = 1;
+	Game.customStatsMenu.push(function(){
+		CCSE.AppendStatsVersionNumber("TrixCookies", TrixCookies.version);
+	});'z
+	
+	last = CCSE.NewAchievement('Sweet Science', 'Harvest <b>5</b> bifurcated sugar lumps.', [1, 1, iconsURL]); 
+	last = CCSE.NewAchievement('Sugar Atom Splitter', 'Harvest <b>15</b> bifurcated sugar lumps.', [1, 2, iconsURL]); 
+	last = CCSE.NewAchievement('Schizomainac', 'Harvest <b>50</b> bifurcated sugar lumps.', [1, 3, iconsURL]); 
+	TrixCookies.isLoaded = 1;
 };
 
 if (!TrixCookies.isLoaded) {
-    if (CCSE && CCSE.isLoaded) {
+	if (CCSE && CCSE.isLoaded) {
         TrixCookies.launch();
     } else {
         if (!CCSE) var CCSE = {};
