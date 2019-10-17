cc20b5b147608c4bf415a80a77766eb6d6abb085
Alex Trix <trixter9994@gmail.com>
2019-10-17 10:17:20 -0400

    TrixCookies v0.1
    Beautified code and gave player third party achievement.

--- TrixCookies.js
+++ TrixCookies.js
@@ -1,5 +1,6 @@
-if(TrixCookies === undefined) var TrixCookies = {};
-if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+Game.Win('Third-party');
+if (TrixCookies === undefined) var TrixCookies = {};
+if (typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 
 TrixCookies.launch = function(){
 	Game.customOptionsMenu.push(function(){
@@ -9,17 +10,15 @@ TrixCookies.launch = function(){
 Game.customStatsMenu.push(function(){
 	CCSE.AppendStatsVersionNumber("TrixCookies", "0.1");
 });
+    TrixCookies.isLoaded = 1;
+};
 
-TrixCookies.isLoaded = 1; 
-}
-
-if(!TrixCookies.isLoaded){
-	if(CCSE && CCSE.isLoaded){
-		TrixCookies.launch();
-	}
-	else{
-		if(!CCSE) var CCSE = {};
-		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
-		CCSE.postLoadHooks.push(TrixCookies.launch);
-	}
+if (!TrixCookies.isLoaded) {
+    if (CCSE && CCSE.isLoaded) {
+        TrixCookies.launch();
+    } else {
+        if (!CCSE) var CCSE = {};
+        if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+        CCSE.postLoadHooks.push(TrixCookies.launch);
+    }
 }
